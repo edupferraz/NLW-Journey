9aa203d420cc322f177c925a4878b50e02613e2c
Eduardo Ferraz <edupferraz@gmail.com>
2024-07-14 13:42:31 -0300

    Class 02 - Relevant docs and Supervisor

--- chatComp.py
+++ chatComp.py
@@ -4,11 +4,9 @@ client = OpenAI()
 response = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Who won the world series in 2020?"},
-    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-    {"role": "user", "content": "Where was it played?"}
+{"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Vou viajar para Londres em agosto de 2024. Quero que faça um roteiro de viagens para mim."},
   ]
 )
 
-print(response.choices[0].message)
+print(response.choices[0].message.content)
